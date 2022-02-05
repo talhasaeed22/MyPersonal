f311f806ee104d85970d5df685d3f734c812fa3d
M.Talha Saeed <talhasaeed100@gmail.com>
2022-02-06 01:24:57 +0500

    navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,7 +7,13 @@ function Navbar(props) {
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a id="logo" className="navbar-brand" href="/"> <img src={logo} alt="" width="30" height="30" className="d-inline-block align-text-top" />PEROSNAL</a>
+                    <a id='logo' class="navbar-brand" href="/">
+                        <img src={logo} alt="" width="30" height="24" class="d-inline-block align-text-top" />
+                        Personal
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav m-auto mb-2 mb-lg-0">
                             <li className="nav-item mx-4">
@@ -27,8 +33,8 @@ function Navbar(props) {
                             </li>
                         </ul>
                         <div className="form-check form-switch text-light mx-3 ">
-                            <input className="form-check-input" type="checkbox" onClick={props.switchMode} role="switch" id="flexSwitchCheckDefault"/>
-                            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.mode === 'light'?'Enable Dark Mode':'Enable Light Mode'}</label>
+                            <input className="form-check-input" type="checkbox" onClick={props.switchMode} role="switch" id="flexSwitchCheckDefault" />
+                            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{props.mode === 'light' ? 'Enable Dark Mode' : 'Enable Light Mode'}</label>
                         </div>
                     </div>
                 </div>
