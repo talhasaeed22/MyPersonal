3986cbe31401eabacf5f133b3cf15eb08748101c
M.Talha Saeed <talhasaeed100@gmail.com>
2022-02-06 01:03:14 +0500

    second commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,23 +7,23 @@ function Navbar(props) {
         <>
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a id="logo" className="navbar-brand" href="#"> <img className='mx-2' src={logo} alt="" width="30" height="30" className="d-inline-block align-text-top" />PEROSNAL</a>
+                    <a id="logo" className="navbar-brand" href="/"> <img src={logo} alt="" width="30" height="30" className="d-inline-block align-text-top" />PEROSNAL</a>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav m-auto mb-2 mb-lg-0">
                             <li className="nav-item mx-4">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
                             </li>
                             <li className="nav-item mx-4">
-                                <a className="nav-link " aria-current="page" href="#">About</a>
+                                <a className="nav-link " aria-current="page" href="/">About</a>
                             </li>
                             <li className="nav-item mx-4">
-                                <a className="nav-link " aria-current="page" href="#">Interests</a>
+                                <a className="nav-link " aria-current="page" href="/">Interests</a>
                             </li>
                             <li className="nav-item mx-4">
-                                <a className="nav-link " aria-current="page" href="#">Education</a>
+                                <a className="nav-link " aria-current="page" href="/">Education</a>
                             </li>
                             <li className="nav-item mx-4">
-                                <a className="nav-link " aria-current="page" href="#">Experience</a>
+                                <a className="nav-link " aria-current="page" href="/">Experience</a>
                             </li>
                         </ul>
                         <div className="form-check form-switch text-light mx-3 ">
